1ed3d1b1fe905ad4972f67773442702a552d9fce
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-01-28 22:25:57 +0000

    classmethod and staticmethod

--- cours_class.py
+++ cours_class.py
@@ -10,9 +10,20 @@ class PlayerCharacter:
         print('Run')
         return 'done'
 
+    # Methode for the class itself
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return num1 + num2
+    
+    # Method for the class itself that not care the constructor attribute
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
 
 player1 = PlayerCharacter('Alou', 20)
 player2 = PlayerCharacter('Mateo', 90)
 print(player1.name)
 print(player1.age)
 print(player1.membership)
+print(player1.adding_things(5, 10))
+print(PlayerCharacter.adding_things(1, 8))
