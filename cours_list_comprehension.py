6fbcd344cf608ae707c438d23eb0fe9646e28cad
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-02-17 23:40:37 +0000

    dictionary comprehension

--- cours_list_comprehension.py
+++ cours_list_comprehension.py
@@ -1,4 +1,6 @@
 # list, set, dictionary
+
+# list comprehension
 my_list = [char for char in 'hello']
 my_list2 = [num for num in range(0, 100)]
 my_list3 = [num**2 for num in range(0, 100)]
@@ -8,3 +10,13 @@ print(my_list)
 print(my_list2)
 print(my_list3)
 print(my_list4)
+
+# dictionar comprehension
+simple_dict = {
+    'a': 1,
+    'b': 2
+}
+
+my_dict = {k: v**2 for k, v in simple_dict.items() if v % 2 == 0}
+print(my_dict)
+ 
